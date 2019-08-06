1a84d81acdf44b0371d823b4e6fdcc0371f36e5e
Julia <julsuniverse@gmail.com>
2019-08-06 22:33:30 +0300

    remove debug info

--- resources/views/user/cabinet/index.blade.php
+++ resources/views/user/cabinet/index.blade.php
@@ -1,8 +1,6 @@
 @extends('layout.main')
 
 @section('content')
-    {{ session('error')}}
-    {{ $errors->has('password') }}
     <h3 class="text-left">Добро пожаловать, {{ $user->name }}</h3>
     <div class="">
         <nav>
