04469c1c9bc0dbc45c8e518cb83b65dd495c1835
Julia <julsuniverse@gmail.com>
2020-08-09 22:38:51 +0300

    change oracle title en

--- resources/lang/en/random-card.php
+++ resources/lang/en/random-card.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'taro-title' => 'Interactive Tarot Oracle',
-    'lenormand-title' => 'Interactive Lenormand Oracle',
+    'taro-title' => 'Tarot Oracle',
+    'lenormand-title' => 'Lenormand Oracle',
     'tarot1' => 'TAROT CARD for TODAY',
     'tarot2' => 'TAROT ADVICE for TODAY',
     'tarot3' => 'LOVE MATTERS ADVICE',
@@ -11,6 +11,6 @@ return [
     'runes-btn' => 'Runes',
     'empty-deck' => 'The deck is empty',
     'tarot-small' => 'Cards\' interpretation for this oracle is made by the author of this website',
-    'lenormand-small' => 'Cards\' interpretation for this Lenormand oracle is made by the author of this website',
-    'lenormand-intro' => 'Maria Lenormand’s Illustrious Cards unveil your nearest future'
+    'lenormand-small' => 'Cards\' interpretation for this oracle is made by the author of this website',
+    'lenormand-intro' => 'Maria Lenormand’s Cards unveil your nearest future'
 ];
