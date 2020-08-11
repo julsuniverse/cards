4024a0bd12068e6f78b2238771ce6d5688d2bf3e
Julia <julsuniverse@gmail.com>
2020-08-11 21:49:46 +0300

    fix margin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -97,7 +97,7 @@
         <div class="row text-center mt-2">
             <div class="col-12 mb-1">
                 <h2>{{ __('random-card.taro-title') }}</h2>
-                <div class="text-small text-center font-weight-bold">{{ __('random-card.tarot-small') }} </div>
+                <div class="text-small text-center font-weight-bold mb-1">{{ __('random-card.tarot-small') }} </div>
                 <div class="row mt-md-2">
                     <div class="col-sm-12 col-md-4">
                         <span class="text-center">
